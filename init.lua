8adf73bd0fb8420a58523cb6db441774736eb05f
1234567899873465 <madeforadiscordbot@gmail.com>
2025-07-08 09:16:30 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ if oh then
 end
 
 local web = true
-local user = "Upbolt" -- change if you're using a fork
+local user = "1234567899873465" -- change if you're using a fork
 local branch = "revision"
 local importCache = {}
 
